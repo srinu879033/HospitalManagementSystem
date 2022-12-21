6ac0370ba4f3ffbda3d6cb95c2d9acc32d4947ba
srinu879033 <srinu879033@gmail.com>
2022-12-21 17:38:46 +0530

    gh pages

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,4 +1,4 @@
-const PRODUCTION_URL = "";
+const PRODUCTION_URL = "https://hospitalmanagementbackend.vercel.app/";
 const TEST_URL = "http://localhost:3001";
 const BASE_URL = TEST_URL;
 export default BASE_URL;
